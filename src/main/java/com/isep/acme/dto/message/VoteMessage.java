468a0381584901442a57e5439d8448c3827977d2
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2023-04-06 22:56:08 +0100

    Fixed VoteMessage

--- src/main/java/com/isep/acme/dto/message/VoteMessage.java
+++ src/main/java/com/isep/acme/dto/message/VoteMessage.java
@@ -4,9 +4,11 @@ import com.isep.acme.domain.model.enumerate.VoteType;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class VoteMessage {
     private Long voteId;
